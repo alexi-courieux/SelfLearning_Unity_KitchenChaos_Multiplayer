6adc6eafd59d81eaf8ebceabc3c0719e2d843cb0
Alexi Courieux <courieuxa@gmail.com>
2024-09-21 15:57:58 -0400

    MP Trash counter https://youtu.be/7glCsF9fv3s?t=7208

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -56,9 +56,12 @@ public class KitchenObject : NetworkBehaviour {
     }
 
     public void DestroySelf() {
-        kitchenObjectParent.ClearKitchenObject();
-
         Destroy(gameObject);
+    }
+
+    public void ClearKitchenObjectOnParent()
+    {
+        kitchenObjectParent.ClearKitchenObject();
     }
 
     public bool TryGetPlate(out PlateKitchenObject plateKitchenObject) {
@@ -76,6 +79,11 @@ public class KitchenObject : NetworkBehaviour {
     public static void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
     {
         KitchenGameMultiplayer.Instance.SpawnKitchenObject(kitchenObjectSO, kitchenObjectParent);
+    }
+    
+    public static void DestroyKitchenObject(KitchenObject kitchenObject)
+    {
+        KitchenGameMultiplayer.Instance.DestroyKitchenObject(kitchenObject);
     }
 
 }
